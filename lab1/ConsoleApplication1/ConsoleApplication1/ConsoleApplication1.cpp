6710c8db3c5a8638d0c16f1bd1ba8c551eb494bb
tjarrow <nickita0104@gmail.com>
2019-01-30 15:45:38 +0300

    Initial commit

---
+++ lab1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+long double fact(int N)
+{
+	if (N < 0) 
+		return 0; 
+	if (N == 0)
+		return 1;
+	else 
+		return N * fact(N - 1); 
+}
+
+int main()
+{
+	int N;
+	setlocale(0, ""); 
+	cout << "Enter number to count factorial: ";
+	cin >> N;
+	cout << "Factorial for number: " << N << " = " << fact(N) << endl << endl; 
+	return 0;
+}
