daa2dcb2ec17273d55f7b84ffa52e76f1c9568c5
nikita <nickita0104@gmail.com>
2019-02-24 23:25:28 +0300

    Implementation of watershed algorithm

--- Seeds/watershed.h
+++ Seeds/watershed.h
@@ -1,5 +1,11 @@
 #ifndef WATERSHED_H
 #define WATERSHED_H
 
+#include <QMainWindow>
+#include <vector>
+
+void watershed(QString path,int color, int color_2, int& amount, std::vector<double>& length, std::vector<double>& width, std::vector<double>& area);
+
+
 #endif // WATERSHED_H
 
