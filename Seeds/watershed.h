6248224bc682097d15406afed9bc32106ce7b29c
nikita <nickita0104@gmail.com>
2019-03-19 18:24:22 +0300

    Define pixel size for watershed algorithm

--- Seeds/watershed.h
+++ Seeds/watershed.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <vector>
 
+#define PIXEL_SIZE 0.265
+
 void watershed(QString path,int color, int color_2, int& amount, std::vector<double>& length, std::vector<double>& width, std::vector<double>& area);
 
 
