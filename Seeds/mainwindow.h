cb2a64a86fcdd50b81f8df702ca271ab47338003
nikita <nickita0104@gmail.com>
2019-03-19 18:21:03 +0300

    Initialize methods for main window

--- Seeds/mainwindow.h
+++ Seeds/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+
 private:
     Ui::MainWindow *ui;
+    void addRows(int count);
+    QGraphicsScene *scene;
+    QGraphicsPixmapItem *pixmap;
 };
 
 #endif // MAINWINDOW_H
